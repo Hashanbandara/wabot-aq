a8faf6fbfdf6e21ac8254bd2bbe43684f3763b45
Dasun Rasanjana <83932875+DasunRasanjana0@users.noreply.github.com>
2021-05-20 23:54:07 +0530

    Update setwelcome.js

--- plugins/setwelcome.js
+++ plugins/setwelcome.js
@@ -3,10 +3,10 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
     if (isROwner) global.conn.welcome = text
     else if (isOwner) conn.welcome = text
     else global.DATABASE._data.chats[m.chat].sWelcome = text
-    m.reply('Welcome berhasil diatur\n@user (Mention)\n@subject (Judul Grup)')
+    m.reply('පිලිගැනීම සාර්තකව සකසා ඇත🇱🇰\n@user (අලුතෙන් ආපු කෙනා මෙන්ශන් වේ.)\n@subject (ඔබ සදහන් කල පනිවිඩය.)')
   } else throw 'Teksnya mana?'
 }
-handler.help = ['setwelcome <teks>']
+handler.help = ['setwelcome <text>']
 handler.tags = ['owner', 'group']
 
 handler.command = /^setwelcome$/i
