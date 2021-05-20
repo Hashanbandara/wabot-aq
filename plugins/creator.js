1f69286d83652b9735bb8f65849c4454e7a6d67f
Dasun Rasanjana <83932875+DasunRasanjana0@users.noreply.github.com>
2021-05-21 00:24:02 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '94718790419', 'DASUN RASANJANA', m)
-  this.sendContact(m.chat, '0', '+94718790419', m)
+  this.sendContact(m.chat, '+94718790419', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
